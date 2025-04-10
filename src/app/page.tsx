8ac4885d1bd752b2d42c2603dc55b5b38cb0e60d
unknown <gabrielwedig2@gmail.com>
2025-04-09 22:47:32 -0300

    Filters finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,23 +17,37 @@ import {
 } from '@/components/ui/select'
 import { firstUpper } from '@/lib/utils'
 import { useRouter } from 'next/navigation'
-import { useEffect, useState } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
 import { generations, types } from './filters'
 import axios from 'axios'
+import { Input } from '@/components/ui/input'
+import { Controller, useForm } from 'react-hook-form'
 
 interface Pokemon {
   name: string
   url: string
 }
 
+interface Filter {
+  search: string
+  generation: string
+  type: string
+}
+
 export default function Pokemons() {
   const router = useRouter()
+  const defaultTitle = 'Pokémons'
 
-  const [title, setTitle] = useState<string>('Pokémons')
+  const [title, setTitle] = useState<string>(defaultTitle)
   const [page, setPage] = useState<number>(1)
   const [pokemons, setPokemons] = useState<Pokemon[]>([])
 
   useEffect(() => {
+    const allPokemons = getCachePokemons()
+
+    if (allPokemons.length) {
+      setPokemons(allPokemons)
+    }
     getPokemons()
   }, [])
 
@@ -53,6 +67,7 @@ export default function Pokemons() {
       'https://pokeapi.co/api/v2/pokemon?offset=0&limit=2000'
     )
     setPokemons(data.results)
+    localStorage.setItem('allPokemons', JSON.stringify(data.results))
   }
 
   const limit = 10
@@ -80,39 +95,106 @@ export default function Pokemons() {
     setPokemons(data.pokemon.map((p: any) => p.pokemon))
   }
 
+  const handleSearch = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value.toLowerCase()
+    const pokemonsList: Pokemon[] = getCachePokemons()
+    const filtered = pokemonsList.filter((pokemon) =>
+      pokemon.name.toLowerCase().startsWith(value)
+    )
+    setPokemons(filtered)
+  }
+
+  const getCachePokemons = () => {
+    const allPokemons = localStorage.getItem('allPokemons')
+    if (allPokemons) {
+      return JSON.parse(allPokemons)
+    }
+    return []
+  }
+
+  const handleReset = () => {
+    const allPokemons = getCachePokemons()
+    setPokemons(allPokemons)
+    setTitle(defaultTitle)
+    reset()
+  }
+
+  const { register, reset, control } = useForm<Filter>({
+    defaultValues: {
+      search: '',
+      generation: '',
+      type: ''
+    }
+  })
+
   return (
     <section className="px-80 py-10">
       <div className="flex justify-between">
         <h1 className="text-4xl font-semibold mb-10">{title}</h1>
-        <div className="flex gap-2">
-          <Select onValueChange={handleGeneration}>
-            <SelectTrigger className="w-[150px]">
-              <SelectValue placeholder="Geração" />
-            </SelectTrigger>
-            <SelectContent>
-              {generations.map((generation, i) => (
-                <SelectItem key={i} value={generation.value}>
-                  {generation.name}
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </Select>
-          <Select onValueChange={handleType}>
-            <SelectTrigger className="w-[150px]">
-              <SelectValue placeholder="Tipo" />
-            </SelectTrigger>
-            <SelectContent>
-              {types.map((type, i) => (
-                <SelectItem key={i} value={type}>
-                  {firstUpper(type)}
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </Select>
-        </div>
+        <form className="flex gap-2">
+          <Input
+            placeholder="Pesquisar"
+            {...register('search', {
+              onChange: handleSearch
+            })}
+          />
+          <Controller
+            control={control}
+            name="generation"
+            render={({ field }) => (
+              <Select
+                onValueChange={(value) => {
+                  field.onChange(value)
+                  handleGeneration(value)
+                }}
+                value={field.value}
+              >
+                <SelectTrigger className="w-[150px]">
+                  <SelectValue placeholder="Geração" />
+                </SelectTrigger>
+                <SelectContent>
+                  {generations.map((generation, i) => (
+                    <SelectItem key={i} value={generation.value}>
+                      {generation.name}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            )}
+          />
+          <Controller
+            control={control}
+            name="type"
+            render={({ field }) => (
+              <Select
+                onValueChange={(value) => {
+                  field.onChange(value)
+                  handleType(value)
+                }}
+                value={field.value}
+              >
+                <SelectTrigger className="w-[150px]">
+                  <SelectValue placeholder="Tipo" />
+                </SelectTrigger>
+                <SelectContent>
+                  {types.map((type, i) => (
+                    <SelectItem key={i} value={type}>
+                      {firstUpper(type)}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            )}
+          />
+
+          <Button type="button" variant="ghost" onClick={handleReset}>
+            Limpar
+          </Button>
+        </form>
       </div>
 
-      <ul className="flex flex-col gap-2 mb-10">
+      <ul className="flex flex-col items-center gap-2 min-h-[calc(100vh-160px)]">
+        {paginated.length === 0 && <p>Pokémon não encontrado.</p>}
         {paginated.map((pokemon, i) => (
           <li
             key={i}
